a9cd351abae9398d95fb3522e750d4d1fbd3d8e1
Matt Brown <270025966@bun2-206-22.acg.ac.nz>
2021-06-16 12:02:43 +1200

    first commit

--- js/custom.js
+++ js/custom.js
@@ -9,25 +9,92 @@
   getUpdatebutton.onclick = function(){
 
     var getNumberPeople = document.getElementById('people');
-    var getNumberNights = document.getElementById('nights');
+    var getNumberNights = parseInt(document.getElementById('nights').value);
 
-    console.log(getNumberPeople.value);
-    var getLeft = document.getElementById('left');
+
+    console.log(typeof getNumberNights);
+
+    console.log(getNumberNights);
+    var getFirst = document.getElementById('first');
+    var getSecondOption = document.getElementById('second');
     var getThirdOption = document.getElementById('third');
+    var getFourthOption = document.getElementById('fourth');
 
     console.log('working');
 
-    if (getNumberPeople.value === 'House' + getNumberNights.value >= 15)  {
+    if (getNumberPeople.value === '1')  {
       console.log('People');
-      document.getElementById("left").style.display = "block";
-      document.getElementById("left").style.display = "none";
+      document.getElementById("second").style.opacity = "0.5";
+      document.getElementById("third").style.opacity = "0.5";
+      document.getElementById("fourth").style.opacity = "0.5";
+      document.getElementById("first").style.opacity = "1";
     }
 
-    if (getNumberPeople.value === 'Hotel')  {
+
+    if (getNumberPeople.value === '2' )  {
       console.log('People');
-      document.getElementById("third").style.display = "block";
-      document.getElementById("third").style.display = "none";
+      document.getElementById("first").style.opacity = "1";
+      document.getElementById("second").style.opacity = "1";
+      document.getElementById("third").style.opacity = "1";
+      document.getElementById("fourth").style.opacity = "1";
     }
+
+    if (getNumberPeople.value === '3')  {
+      console.log('People');
+      document.getElementById("first").style.opacity = "0.5";
+      document.getElementById("second").style.opacity = "1";
+      document.getElementById("third").style.opacity = "1";
+      document.getElementById("fourth").style.opacity = "1";
+    }
+
+    if (getNumberPeople.value === '4')  {
+      console.log('People');
+      document.getElementById("second").style.opacity = "1";
+      document.getElementById("third").style.opacity = "1";
+      document.getElementById("fourth").style.opacity = "1";
+      document.getElementById("first").style.opacity = "0.5";
+    }
+
+    if (getNumberNights > 2){
+      document.getElementById("first").style.opacity = "0.5";
+      document.getElementById("second").style.opacity = "1";
+      document.getElementById("fourth").style.opacity = "1";
+      document.getElementById("third").style.opacity = "1";
+
+    }
+
+    if (getNumberNights < 3){
+      document.getElementById("first").style.opacity = "0.5";
+      document.getElementById("second").style.opacity = "1";
+      document.getElementById("fourth").style.opacity = "1";
+      document.getElementById("third").style.opacity = "1";
+
+    }
+
+    if (getNumberNights < 5){
+      document.getElementById("first").style.opacity = "1";
+      document.getElementById("second").style.opacity = "1";
+      document.getElementById("fourth").style.opacity = "1";
+      document.getElementById("third").style.opacity = "1";
+
+    }
+
+    if (getNumberNights > 5 ){
+      document.getElementById("first").style.opacity = "0.5";
+      document.getElementById("second").style.opacity = "0.5";
+      document.getElementById("fourth").style.opacity = "1";
+      document.getElementById("third").style.opacity = "1";
+
+    }
+
+    if (getNumberNights > 10){
+      document.getElementById("first").style.opacity = "0.5";
+      document.getElementById("second").style.opacity = "0.5";
+      document.getElementById("fourth").style.opacity = "1";
+      document.getElementById("third").style.opacity = "0.5";
+
+    }
+
 
   };
   // onclick ENDS
