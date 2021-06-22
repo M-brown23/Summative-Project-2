cbd5d2e91f5fcf1b10807df5a9e1ef6c136470bf
Matt Brown <270025966@bun2-206-22.acg.ac.nz>
2021-06-22 12:28:13 +1200

    swag

--- js/custom.js
+++ js/custom.js
@@ -15,8 +15,13 @@
       maxPpl: 2,
       price: 157,
       minNight: 1,
-      maxNight: 5
+      maxNight: 5,
+      menu: 'Hash Browns & Coffee',
+      menuStacked: {
+        coffee: 'something..'
+      }
     },
+
     hostel: {
 
       minPpl: 1,
@@ -25,6 +30,7 @@
       maxNight: 10,
 
     },
+
     motel: {
 
       minPpl: 2,
@@ -34,6 +40,7 @@
       maxNight: 10,
 
     },
+
     house: {
 
       minPpl: 1,
@@ -131,19 +138,183 @@
 
     }
 
-    var total = document.getElementById('total')
+
 
     var x = getNumberNights;
     var y = obj.motel.price;
-    var sum = x * y;
+    var motelSum = x * y;
     // System.out.println(total); // Print the sum of x + y
-    console.log(sum);
+    console.log(motelSum);
+
+
+
+    var x = getNumberNights;
+    var y = obj.hotel.price;
+    var hotelSum = x * y;
+    // System.out.println(total); // Print the sum of x + y
+    console.log(hotelSum);
+
+
+
+    var x = getNumberNights;
+    var y = obj.hostel.price;
+    var hostelSum = x * y;
+    // System.out.println(total); // Print the sum of x + y
+    console.log(hostelSum);
+
+
+
+
+    var x = getNumberNights;
+    var y = obj.motel.price;
+    var motelSum = x * y;
+    // System.out.println(total); // Print the sum of x + y
+    console.log(motelSum);
+
+
+
+    var x = getNumberNights;
+    var y = obj.house.price;
+    var houseSum = x * y;
+    // System.out.println(total); // Print the sum of x + y
+    console.log(typeof houseSum);
+
+  // var total = document.getElementById("total").textContent;
+  // document.getElementById("total") = houseSum;
+
+  // var str = document.getElementById("total").innerHTML;
+  // var res = str.replace("0", houseSum);
+  // document.getElementById("total").innerHTML = res;
+
+  document.getElementById("housetotal").textContent =  '$' + houseSum;
+  document.getElementById("hoteltotal").textContent =  '$' + hotelSum;
+  document.getElementById("moteltotal").textContent =  '$' + motelSum;
+  document.getElementById("hosteltotal").textContent = '$' + hostelSum;
+
+  document.getElementById("housenights").textContent = getNumberNights + ' nights selected ';
+  document.getElementById("hotelnights").textContent = getNumberNights + ' nights selected ';
+  document.getElementById("motelnights").textContent = getNumberNights + ' nights selected ';
+  document.getElementById("hostelnights").textContent = getNumberNights + ' nights selected';
+
+  document.getElementById("hostelpernights").textContent = '$ ' + obj.hostel.price + ' per night'
+  document.getElementById("motelpernights").textContent = '$ ' + obj.motel.price + ' per night'
+  document.getElementById("hotelpernights").textContent = '$ ' + obj.hotel.price + ' per night'
+  document.getElementById("housepernights").textContent = '$ ' + obj.house.price + ' per night'
 
   };
   // onclick ENDS
 
 
+  // Items Magic [Preview-1]
+  $('.paper.preview-1 .item').click(function () {
+      $('.paper.preview-1 .item').not(this).removeClass('active');
+      $(this).toggleClass('active');
+      if ($('.paper.preview-1 .item').hasClass('active')) {
+          $('.paper.preview-1').addClass('item-active');
+      } else {
+          $('.paper.preview-1').removeClass('item-active')
+      };
+  });
+  // Preventing Closing when Click inside counter
+  $('.minus,.plus').click(function (a) {
+      a.stopPropagation();
+  });
 
+  // Adding Counter [Preview-1]
+  $('.paper.preview-1 .plus').click(function () {
+      $(this).parent().find('.number').html(function (y, val) {
+          return val * 1 + 1
+      });
+  });
+
+  $('.paper.preview-1 .minus').click(function () {
+      var increased = parseInt($(this).parent().find('.number').text());
+      if (isNaN(increased) || increased > 0) {
+          $(this).parent().find('.number').html(function (t, val) {
+              return val * 1 - 1
+          });
+      } else {
+        return false;
+      }
+  });
+  $('.paper.preview-1 .plus, .paper.preview-1 .minus').click(function () {
+      var increased = parseInt($(this).parent().find('.number').text());
+
+      var itemsTotal = 0;
+      $('.paper.preview-1 .number').each(function () {
+          itemsTotal += ($(this).html() * 1);
+      });
+      $('.paper.preview-1 .items-total').html(itemsTotal);
+
+      var itemsTotalAll = parseInt($('.paper.preview-1 .items-total').text());
+      if (isNaN(itemsTotalAll) || itemsTotalAll <= 0) {
+          $('.paper.preview-1 .action').hide();
+      } else {
+          $('.paper.preview-1 .action').show();
+      }
+
+      if (isNaN(increased) || increased <= 0) {
+          $(this).parent().find('.number').hide();
+      } else {
+          $(this).parent().find('.number').show();
+      }
+  });
+
+  // JavaScript code is done
+  // the rest are just repeating
+  // for Paper Preview-2 functionality
+
+  // Items Magic [Preview-2]
+  $('.paper.preview-2 .item').click(function () {
+      $('.paper.preview-2 .item').not(this).removeClass('active');
+      $(this).toggleClass('active');
+      if ($('.paper.preview-2 .item').hasClass('active')) {
+          $('.paper.preview-2').addClass('item-active');
+      } else {
+          $('.paper.preview-2').removeClass('item-active')
+      };
+  });
+
+  // Adding Counter [Preview-2]
+  $('.paper.preview-2 .plus').click(function () {
+      $(this).parent().find('.number').html(function (p, val) {
+          return val * 1 + 1
+      });
+  });
+
+
+  $('.paper.preview-2 .minus').click(function () {
+      var increased = parseInt($(this).parent().find('.number').text());
+      if (isNaN(increased) || increased > 0) {
+          $(this).parent().find('.number').html(function (k, val) {
+              return val * 1 - 1
+          });
+      } else {
+
+      }
+  });
+  $('.paper.preview-2 .plus, .paper.preview-2 .minus').click(function () {
+      var increased = parseInt($(this).parent().find('.number').text());
+
+      var itemsTotal = 0;
+      $('.paper.preview-2 .number').each(function () {
+          itemsTotal += ($(this).html() * 1);
+      });
+      $('.paper.preview-2 .items-total').html(itemsTotal);
+
+      var itemsTotalAll = parseInt($('.paper.preview-2 .items-total').text());
+      if (isNaN(itemsTotalAll) || itemsTotalAll <= 0) {
+          $('.paper.preview-2 .action').hide();
+      } else {
+          $('.paper.preview-2 .action').show();
+      }
+
+      if (isNaN(increased) || increased <= 0) {
+          $(this).parent().find('.number').hide();
+      } else {
+          $(this).parent().find('.number').show();
+      }
+  });
 
 
 }());
